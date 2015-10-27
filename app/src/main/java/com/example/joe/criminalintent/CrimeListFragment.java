b5e10acdc38c5a3683f316006098a8fc2be3170f
Joe Stevens <joseph.stevens34@gmail.com>
2015-10-27 00:41:39 -0400

    End of 13.15

--- app/src/main/java/com/example/joe/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/joe/criminalintent/CrimeListFragment.java
@@ -3,6 +3,7 @@ package com.example.joe.criminalintent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -138,12 +139,7 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_CRIME){
 
-        }
-    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -161,9 +157,22 @@ public class CrimeListFragment extends Fragment {
                         crime.getID());
                 startActivity(intent);
                 return true;
+            case R.id.menu_item_show_subtitle:
+                updateSubtitle();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    private void updateSubtitle(){
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        int crimeCount = crimeLab.getCrimes().size();
+        String subtitle = getString(R.string.subtitle_format,
+                crimeCount);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setSubtitle(subtitle);
     }
 }
